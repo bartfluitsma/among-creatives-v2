41eab46cf96a2e46cc619db8f0724ad390b7986a
Bart Fluitsmaa <bartfluitsma@gmail.com>
2022-06-24 13:25:24 +0200

    Added google G-tag for analytics, removed 1 photo of food

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,20 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-netlify`,
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-2LQE30PX68", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
